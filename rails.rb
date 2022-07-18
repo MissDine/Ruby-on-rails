c20f70af4c4436c5c2e848f889a6eb3d60284212
MissDine <enidnyatichi1212@gmail.com>
2022-07-18 11:17:39 +0300

    Feature edit

---
+++ rails.rb
@@ -0,0 +1,5 @@
+# Ruby on Rails was created by David Heinemeier Hansson in 2004. 
+# A web framework: A web framework provides developers the tools they need in order to build applications. 
+# A Ruby Gem: At its core, Ruby on Rails is simply a set of Ruby code libraries, and since the entire codebase is open source you have the ability to review the framework to better understand how it works.
+# An MVC framework: MVC stands for Model-View-Controller. MVC is a popular application architecture that helps developers naturally separate concerns and organize their applications properly. 
+# An opinionated framework: one way Rails speeds up developer productivity is by enforcing strong opinions about how your code should be organized, from folder structure to file and variable naming.
